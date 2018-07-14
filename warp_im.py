341ddb469e45d4b090e7b65bf7234290bc276510
Animesh <35520996+Animesh98@users.noreply.github.com>
2018-07-15 00:38:17 +0530

    warp_im  : Function that uses the transformation matrix to rotate and align the two faces

---
+++ warp_im.py
@@ -0,0 +1,9 @@
+def warp_im(im, M, dshape):
+    output_im = numpy.zeros(dshape, dtype=im.dtype)
+    cv2.warpAffine(im,
+                   M[:2],
+                   (dshape[1], dshape[0]),
+                   dst=output_im,
+                   borderMode=cv2.BORDER_TRANSPARENT,
+                   flags=cv2.WARP_INVERSE_MAP)
+    return output_im
