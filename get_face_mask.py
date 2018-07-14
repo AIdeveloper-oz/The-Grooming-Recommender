3ddaec28c8309ba8087667244dc7e41a9e5f39a8
11shruti <38371483+11shruti@users.noreply.github.com>
2018-07-15 00:35:04 +0530

    get_face_mask function,Returns the mask, from the image to be overlayed

---
+++ get_face_mask.py
@@ -0,0 +1,19 @@
+def draw_convex_hull(im, points, color):
+    points=points.astype(numpy.int32)
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(im, points, color=color)
+
+def get_face_mask(im, landmarks):
+    im = numpy.zeros(im.shape[:2], dtype=numpy.float64)
+
+    for group in OVERLAY_POINTS:
+        draw_convex_hull(im,
+                         landmarks[group],
+                         color=1)
+
+    im = numpy.array([im, im, im]).transpose((1, 2, 0))
+
+    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+
+    return im
